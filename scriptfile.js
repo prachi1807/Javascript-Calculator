ce5dfae2f96db6df77fb1c286b498e08cefb2699
Prachi Chodankar <55678809+prachi1807@users.noreply.github.com>
2020-12-27 12:17:18 +0530

    Update scriptfile.js

--- scriptfile.js
+++ scriptfile.js
@@ -1,29 +1,54 @@
-var number1, number2, sign;
-function callNum(num)
+var a='';
+var b='';
+var ans='';
+var num = [];
+function callNum(digit)
 {
-	document.getElementById("dis").innerHTML= num;
-	switch(num)
+	num.push(digit);
+
+	if(num.length != 1)
 	{
-		case "+": sign=1; break;
-		case "-": sign=2; break;
-		case "*": sign=3; break;
-		case "/": sign=4;
+		a='';
+		document.getElementById('dis').innerHTML = a;	// clearing the screen.
 	}
-	if(number1==undefined)
-		number1=num;
-	else
-		number2=num;
+
+	for(i=0; i<num.length; i++)
+	{
+		a=a+num[i];	// concatenate the elements of the array "num" into a single string, which will be displayed on the screen
+	}
+
+	document.getElementById('dis').innerHTML = a;	
 }
+
 function calculate()
 {
-	if(sign==1)
-		document.getElementById("dis").innerHTML = Number(number1)+Number(number2);
-	else if(sign==2)
-		document.getElementById("dis").innerHTML = Number(number1)-Number(number2);
-	else if(sign==3)
-		document.getElementById("dis").innerHTML = Number(number1)*Number(number2);
-	else
-		document.getElementById("dis").innerHTML = Number(number1)/Number(number2);
-	number1= undefined;
-	number2= undefined;
+	document.getElementById('dis').innerHTML = '';
+
+	// for(i=0; i<num.length ; i++)
+	// {
+	// 	b += num[i];						// concatenating the array "num" into a single string
+	// }
+
+	ans = eval(a);	
+
+	document.getElementById('dis').innerHTML = ans;		// result display
+
+	while(num.length > 0)
+	{
+    	num.pop();				// emptying the array "num"
+	}
+
+	num.push(ans.toString());
+
+}
+
+function clrscr(){
+	document.getElementById('dis').innerHTML = '';
+	
+	while(num.length > 0){
+    	num.pop();				// emptying the array "num"
+	}
+
+	a ='';	
+	b ='';
 }
